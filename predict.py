c9c24cb8293cfc8149e2502c7777bfce6c6829bd
hxh1997 <18852892798@163.com>
2023-10-20 12:37:32 +0800

    修改了predict.py 可以直接读文件夹了

--- predict.py
+++ predict.py
@@ -10,7 +10,12 @@ from models.ghostnet import ghostnet
 
 
 def main(classes):
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = ghostnet(num_classes=7).to(device)
+    model_weight_path = "./checkpoints/best.pth"
+    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    model.eval()
 
     data_transform = transforms.Compose(
         [transforms.Resize(256),
@@ -18,37 +23,32 @@ def main(classes):
          transforms.ToTensor(),
          transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
 
-    # load image
-    img_path = "yixue/test/VASC/ISIC_0034814.jpg"
-    assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
-    img = Image.open(img_path)
-    plt.imshow(img)
-    # [N, C, H, W]
-    img = data_transform(img)
-    # expand batch dimension
-    img = torch.unsqueeze(img, dim=0)
+    test_dir = "../Medical classification/test"  # Path to the directory containing test images
+    assert os.path.exists(test_dir), "Directory '{}' does not exist.".format(test_dir)
 
+    total_images = 0
+    correct_predictions = 0
+    for root, dirs, files in os.walk(test_dir):
+        for filename in files:
+            img_path = os.path.join(root, filename)
+            img = Image.open(img_path)
+            img = data_transform(img)
+            img = torch.unsqueeze(img, dim=0)
 
+            with torch.no_grad():
+                output = torch.squeeze(model(img.to(device))).cpu()
+                predict = torch.softmax(output, dim=0)
+                predict_cla = torch.argmax(predict).numpy()
 
-    # create model
-    model = ghostnet(num_classes=7).to(device)
-    # load model weights
-    model_weight_path = "./checkpoints/best.pth"
-    model.load_state_dict(torch.load(model_weight_path, map_location=device))
-    model.eval()
-    with torch.no_grad():
-        # predict class
-        output = torch.squeeze(model(img.to(device))).cpu()
-        predict = torch.softmax(output, dim=0)
-        predict_cla = torch.argmax(predict).numpy()
+            true_label = os.path.basename(root)  # Assuming the folder name is the true class label
+            predicted_label = classes[predict_cla]
 
-    print_res = "class: {}   prob: {:.3}".format(classes[predict_cla],
-                                                 predict[predict_cla].numpy())
-    plt.title(print_res)
-    for i in range(len(predict)):
-        print("class: {:10}   prob: {:.3}".format(classes[i],
-                                                  predict[i].numpy()))
-    plt.show()
+            if true_label == predicted_label:
+                correct_predictions += 1
+            total_images += 1
+
+    accuracy = (correct_predictions / total_images) * 100
+    print("Accuracy: {:.2f}%".format(accuracy))
 
 
 if __name__ == '__main__':
